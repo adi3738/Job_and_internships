df628545149e14fdddbf58157d9f3cc3119fd1a6
adi <adityaasingh3738@gmail.com>
2022-05-23 09:35:21 +0530

    first commit

---
+++ YSM2022_NEW/Home.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace YSM2022_NEW
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        
+
+        protected void Page_Load(object sender, EventArgs e, bool visible, object logoutph)
+        {
+            /*if (Session["userid"] == null)
+            {
+                loginph.Visible = true;
+                logoutph.Visible = false;
+            }
+            else
+            {
+                loginph.Visible = false;
+                logoutph.Visible = true;
+            }*/
+
+        }
+    }
+}
